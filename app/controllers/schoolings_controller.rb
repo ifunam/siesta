c77bcf5ef9da138425ae50eda75695b9441be189
Alejandro Juárez <alex@sc3.fisica.unam.mx>
2008-06-02 19:03:26 -0500

    Minor changes...

--- app/controllers/schoolings_controller.rb
+++ app/controllers/schoolings_controller.rb
@@ -2,6 +2,6 @@ class SchoolingsController < SharedController
   def initialize
     @model = Schooling
     super
-    @columns = %w( degree career school institution studentid credits average startmonth startyear endmonth endyear is_studying_this is_titleholder )
+    @columns = %w( degree career school institution studentid credits average startyear endyear is_studying_this is_titleholder )
   end
 end
