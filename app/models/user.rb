967f981aefa6e42788742c0f76cccb1ae76baea8
Alejandro Juarez <alex@monsterlabs.com.mx>
2010-02-10 18:45:42 -0600

    Adding user_session model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 require 'digest/sha2'
 require 'resolv'
 class User < ActiveRecord::Base
-
   acts_as_authentic do |c|
-   c.logged_in_timeout = 120.minutes # default is 10.minutes
+   c.logged_in_timeout = 120.minutes
   end
-
 end
