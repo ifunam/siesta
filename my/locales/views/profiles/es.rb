0c9823428b0a7dbcab234cc97fb90a3c4b3e6435
Alejandro Juarez <alex@monsterlabs.com.mx>
2010-02-13 20:37:47 -0600

    Initial localization files

---
+++ my/locales/views/profiles/es.rb
@@ -0,0 +1,19 @@
+# encoding: utf-8
+{
+  :es => {
+	:personal_info => 'Información personal',
+	:date_and_place_of_birth => 'Fecha y lugar de nacimiento',
+	:people => {
+		:form => {
+			:firstname => 'Nombre(s)',
+			:lastname1  => 'Apellido Paterno',
+			:lastname2  => 'Apellido Materno',
+			:gender => 'Género',
+			:birthdate => 'Fecha de nacimiento',
+			:country_id => 'País',
+			:state_id => 'Estado',
+			:city_id => 'Ciudad'
+		}
+	}
+  }
+}
