ca9ca6ec4156cb1394f0173aa2c8ffa5ac8e6f46
Alejandro Juarez <alex@monsterlabs.com.mx>
2010-02-13 20:22:56 -0600

    Minor changes for lib/authentication_system.rb and application layout

--- lib/authentication_system.rb
+++ lib/authentication_system.rb
@@ -5,8 +5,7 @@ module AuthenticationSystem
   end
 
   def current_user
-    return @current_user if defined?(@current_user)
-    @current_user = current_user_session
+    @current_user ||= current_user_session.record unless current_user_session.nil?
   end
 
   def store_location
