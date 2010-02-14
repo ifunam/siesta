74bc465e129234e30c8e6e0085a96333987932d9
Alejandro Juarez <alex@monsterlabs.com.mx>
2010-02-14 17:15:51 -0600

    Default scope for role model

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,6 +2,6 @@ class Role < ActiveRecord::Base
   validates_numericality_of :id, :allow_nil => true, :greater_than => 0, :only_integer => true
   validates_presence_of :name
   validates_uniqueness_of :name
-
+  default_scope :order => 'name ASC'
   has_many :user_requests
 end
