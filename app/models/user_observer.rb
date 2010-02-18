2c5ce3c2fe6caf2b73bad7648bbc508b9b181c27
Alejandro Juarez <alex@monsterlabs.com.mx>
2010-02-18 13:24:27 -0600

    Minor changes for user_observer and user_session

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -1,9 +1,9 @@
 class UserObserver < ActiveRecord::Observer
-    def after_create(user)
-	   Notifier.new_user(user).deliver
-	end
+  def after_create(user)
+    Notifier.new_user(user).deliver
+  end
 
-   def after_update(user)
-	   Notifier.new_password(user).deliver unless user.random_password.nil?	
-   end
+  def after_update(user)
+    Notifier.new_password(user).deliver unless user.random_password.nil?	
+  end
 end
