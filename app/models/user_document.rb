8b7061ebf7684a2f624c69f41a8314dfd04d4287
Alejandro Juarez <alex@monsterlabs.com.mx>
2010-02-18 13:21:56 -0600

    Minor change for app/models/user_document

--- app/models/user_document.rb
+++ app/models/user_document.rb
@@ -1,6 +1,6 @@
 class UserDocument < ActiveRecord::Base
   has_attached_file :file
-  validates_attachment_content_type :file, :content_type => ['image/jpeg', 'image/png', 'image/gif', 'application/pdf', 'application/postscript']
+  #validates_attachment_content_type :file, :content_type => ['image/jpeg', 'image/jpg', 'image/png', 'image/gif', 'application/pdf', 'application/postscript']
   validates_attachment_size :file, :max_size => 1..2048
   belongs_to :user
   belongs_to :document
