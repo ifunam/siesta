a59e431d06beb92c330f846824ec817594ffcb4d
Alejandro Juarez <alex@monsterlabs.com.mx>
2010-02-18 13:29:05 -0600

    Adding new notifier

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,20 +1,47 @@
 class Notifier < ActionMailer::Base
-	default :from => "no-reply-siesta@fisica.unam.mx"
-	$subject_prefix = '[SIESTA] - '
+  default :from => "no-reply-siesta@fisica.unam.mx"
+  $subject_prefix = '[SIESTA] - '
 
-	def new_password(user)
-		send_mail_to(user, "New password")
-	end
+  def new_password(user)
+    send_mail_to(user, "Nueva contraseña")
+  end
 
-	def new_user(user)
-		send_mail_to(user, "New account information")
-	end
+  def new_user(user)
+    send_mail_to(user, "Información de nueva cuenta")
+  end
 
-	private
-	def send_mail_to(user, subject)
-		@user = user 
-		mail(:to => user.email, :subject => $subject_prefix + subject) do |format|
-		   format.text
-		end
-	end
+  def student_notification(user)
+    send_mail_to(user, "Su solicitud de Estudiante Asociado fue enviada")
+  end
+
+  def academic_notification(academic_email, user)
+    @user = user 
+    mail(:to => academic_email, :subject => $subject_prefix + 'Solicitud de Estudiante Asociado - IFUNAM') do |format|
+      format.text
+    end
+  end
+
+  def authorized_notification(user)
+    send_mail_to(user, "Solicitud de Estudiante Asociado autorizada")
+  end
+
+  def unauthorized_notification(user)
+    send_mail_to(user, "Solicitud de Estudiante Asociado no autorizada")
+  end
+
+  def new_unix_user(login, password)
+    @login = login
+    @passwd = password
+    mail(:to => 'javo@fisica.unam.mx', :subject => $subject_prefix + 'Nueva cuenta de Estudiante Asociado - IFUNAM') do |format|
+        format.text
+    end
+  end
+
+  private
+  def send_mail_to(user, subject)
+    @user = user 
+    mail(:to => user.email, :subject => $subject_prefix + subject) do |format|
+      format.text
+    end
+  end
 end
