372ff77f9e79e509d6e34d3eabb245ff8fb5ad58
Alejandro Juarez <alex@monsterlabs.com.mx>
2010-03-01 18:35:51 -0600

    Minor changes for admin/photos and admin/students controllers

--- app/controllers/admin/photos_controller.rb
+++ app/controllers/admin/photos_controller.rb
@@ -1,16 +1,17 @@
 class Admin::PhotosController < Academic::ApplicationController 
   layout 'academic'
-
+  respond_to :html
   def edit
     @person = Person.find(params[:id])
+    respond_with(@person)
   end
   
   def update
     @person = Person.find(params[:id])
     if @person.update_attributes(params[:person])
-      redirect_to admin_student_path(:id => @person.user_id)
+      respond_with(@person, :status => :updated, :location => admin_student_path(:id => @person.user_id))
     else
-      redirect_to edit_admin_photo_path(:id => @person.user_id)
+      respond_with(@person, :status => :unprocessable_entity, :location => edit_admin_photo_path(:id => @person.user_id))
     end
   end
 end
