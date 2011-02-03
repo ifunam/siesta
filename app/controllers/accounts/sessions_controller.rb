0295ce610c5a523bb41708fd0a96a649f816df2d
Alejandro Juarez <alex@fisica.unam.mx>
2011-02-03 12:44:38 -0600

    Improvements for views and css for accounts MVC

--- app/controllers/accounts/sessions_controller.rb
+++ app/controllers/accounts/sessions_controller.rb
@@ -1,5 +1,5 @@
 class Accounts::SessionsController < Devise::SessionsController
-  skip_before_filter :authenticate_user!, :only => [:new, :create]
+skip_before_filter :authenticate_user!, :only => [:new, :create]
 
   # POST /resource/sign_in
   def create
