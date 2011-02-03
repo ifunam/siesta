33e57c7d4a9d550c8b010dffdc899bbf12449615
Alejandro Juarez <alex@fisica.unam.mx>
2011-02-03 10:49:48 -0600

    Setting the default url for devise mailers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,6 +22,6 @@ Siesta::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
-  #config.logger = Logger.new(STDOUT)
+  config.action_mailer.default_url_options = { :host => 'siesta.fisica.unam.mx' }
 end
 
