27778766a4a02261cd398447ba1ca5812b3484e9
Alejandro Juarez <alex@fisica.unam.mx>
2011-02-04 12:05:10 -0600

    Solving problems with basic authentication dialog for internet explorer 7x  and devise authentication

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -133,6 +133,7 @@ config.ldap_logger = true
   # If you have any extra navigational formats, like :iphone or :mobile, you
   # should add them to the navigational formats lists. Default is [:html]
   # config.navigational_formats = [:html, :iphone]
+  config.navigational_formats = [:"*/*", :html]
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not (yet) supported by Devise,
