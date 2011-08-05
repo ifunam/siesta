3f073068b4a0403d4877d72d36e87f394b1c7ad6
Alejandro Juarez <juarlex@gmail.com>
2011-08-05 13:59:40 -0500

    is_user_request_authorizable? method addedd to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def blueprint(div_class, options={}, &block)
     content_tag(:div, options.merge(:class => div_class), &block)
   end
+  
+  def is_user_request_authorizable?(user_request)
+    user_request.period.name == Period.activated.name and user_request.requeststatus.id = 3 and user_request.is_official == false and user_request.user.email =~ /fisica.unam.mx/
+  end
 end
