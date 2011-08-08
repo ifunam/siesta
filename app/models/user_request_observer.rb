f0b14846d4fb51b007f9e00ffbd5e98b20ce3c51
Rails Deployment User <deployer@siesta.fisica.unam.mx>
2011-08-08 08:38:29 -0500

    Student requests authorization from manager has been added

--- app/models/user_request_observer.rb
+++ app/models/user_request_observer.rb
@@ -5,9 +5,14 @@ class UserRequestObserver < ActiveRecord::Observer
       Notifier.student_notification(user_request.user).deliver
       Notifier.academic_notification(user_request.user_incharge.email, user_request.user).deliver
     when 3
-      Notifier.authorized_notification(user_request).deliver
-      Notifier.authorized_notification_academic(user_request).deliver
-      Notifier.authorized_notification_academic_coordination(user_request).deliver
+      if user_request.is_official?
+        Notifier.authorization_from_academic_coordination_to_student(user_request).deliver
+        Notifier.authorization_from_academic_coordination_to_academic(user_request).deliver
+      else
+        Notifier.authorized_notification(user_request).deliver
+        Notifier.authorized_notification_academic(user_request).deliver
+        Notifier.authorized_notification_academic_coordination(user_request).deliver
+      end
     when 4    
       Notifier.unauthorized_notification(user_request.user).deliver
       Notifier.unauthorized_notification_academic(user_request.user, user_request.user_incharge).deliver
