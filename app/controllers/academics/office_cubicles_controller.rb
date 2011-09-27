ac852c6c2c562435afcd1702132ecc1afc257aab
Alejandro Juarez <juarlex@gmail.com>
2011-09-27 11:50:48 -0500

    Fixing bugs in academics/office_cubicles MVC

--- app/controllers/academics/office_cubicles_controller.rb
+++ app/controllers/academics/office_cubicles_controller.rb
@@ -1,10 +1,3 @@
-class Academics::OfficeCubiclesController < InheritedResources::Base
-   skip_before_filter :authenticate_user!
-   layout 'academic'
+class Academics::OfficeCubiclesController < Academics::ResourcesController
    defaults :resource_class => OfficeCubicle, :collection_name => 'office_cubicles', :instance_name => 'office_cubicle'
-   
-   def index
-     set_collection_ivar resource_class.paginate(:page => params[:page] ||1, :per_page => params[:per_page] || 10)
-     super
-   end
 end
