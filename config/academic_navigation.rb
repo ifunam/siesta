00fd767541e9cda632626096db055df061519b3f
juarlex <alex@monsterlabs.com.mx>
2012-02-24 12:39:32 -0600

    Disabling office_cubicles MVC for academics

--- config/academic_navigation.rb
+++ config/academic_navigation.rb
@@ -3,6 +3,6 @@
 SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
       primary.item :student_requests, "Solicitudes de estudiantes asociados", academics_student_requests_path
-      primary.item :office_cubicles, "Administración de cubículos", academics_office_cubicles_path
+      #primary.item :office_cubicles, "Administración de cubículos", academics_office_cubicles_path
   end
 end
