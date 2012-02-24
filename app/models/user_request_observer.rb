2cbe4e5d600ede6189008e79fdb6b05f7a82723b
juarlex <alex@monsterlabs.com.mx>
2012-02-24 12:38:41 -0600

    Adding LDAP support in UserRequestObserver and minor changes in EventObserver class

--- app/models/user_request_observer.rb
+++ app/models/user_request_observer.rb
@@ -1,3 +1,4 @@
+require 'clients/aleph_client'
 class UserRequestObserver < ActiveRecord::Observer
   def after_update(user_request)
     case user_request.requeststatus_id
@@ -8,6 +9,18 @@ class UserRequestObserver < ActiveRecord::Observer
       if user_request.is_official?
         Notifier.authorization_from_academic_coordination_to_student(user_request).deliver
         Notifier.authorization_from_academic_coordination_to_academic(user_request).deliver
+        @user = StudentProfile.find(user_request.user.id)
+        @aleph = nil
+        begin
+          @aleph = Aleph::User.find(@user.code)
+        rescue => e
+        end
+        if @aleph.nil?
+          Aleph::User.create(@user.attributes_to_hash)
+        else
+          @aleph.attributes = @user.attributes_to_hash
+          @aleph.update
+        end
       else
         Notifier.authorized_notification(user_request).deliver
         Notifier.authorized_notification_academic(user_request).deliver
