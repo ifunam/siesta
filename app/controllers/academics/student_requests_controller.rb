bc33913b6220b47dc6ce2c96ea08e078255d9cbc
juarlex <alex@monsterlabs.com.mx>
2012-02-24 12:44:14 -0600

    Minor bug fixes in academics/student_requests_controller.rb

--- app/controllers/academics/student_requests_controller.rb
+++ app/controllers/academics/student_requests_controller.rb
@@ -2,7 +2,7 @@ class Academics::StudentRequestsController < Academics::ApplicationController
   respond_to :html, :xml
   def index
     @academic = AcademicClient.find_by_login(current_academics_user.login)
-    @user_requests = UserRequest.find_sent_request_to_user_id(@academic.user_id).all
+    @user_requests = UserRequest.find_sent_request_to_user_id(@academic.id).all
     respond_with(@user_requests)
   end
 
