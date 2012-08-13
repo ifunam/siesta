4a356de9979ace6cf01bc9602af39dc35a912b2f
Alejandro Juarez Robles <juarlex@gmail.com>
2012-08-13 11:22:44 -0500

    Removing session_client code

--- lib/clients/session_client.rb
+++
@@ -1,8 +0,0 @@
-class SessionClient < ActiveResource::Base
-  self.site = 'http://salva.fisica.unam.mx:8080/'
-  self.element_name = "session"
-
-  def self.authenticate?(login, passwd)
-    self.get('login', { :login=> login, :passwd => passwd })['authentication']
-  end
-end
