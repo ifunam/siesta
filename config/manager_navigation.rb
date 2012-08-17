e54f268a51615cc84aa73a0ae9e19992a5737e62
Alejandro Juarez Robles <juarlex@gmail.com>
2012-08-17 11:49:37 -0500

    Basic navigation was added in to the manager layout

--- config/manager_navigation.rb
+++ config/manager_navigation.rb
@@ -1,9 +1,20 @@
 # -*- coding: utf-8 -*-
 # Configures your navigation
 SimpleNavigation::Configuration.run do |navigation|
+  navigation.active_leaf_class = 'active'
+
+  def label_tag(label_name, icon_class=nil)
+    unless icon_class.nil?
+      "<i class=\"#{icon_class}\"></i> " + t("manager_navigation.#{label_name}")
+    else
+      t("manager_navigation.#{label_name}")
+    end
+  end
+
   navigation.items do |primary|
-      primary.item :students, "Solicitudes de estudiantes asociados", managers_students_path
-      primary.item :periods, "Periodos", managers_periods_path
-      #primary.item :office_cubicles, "Administración de cubículos", academics_office_cubicles_path
+    primary.item :students, label_tag(:students), managers_students_path
+    primary.item :periods, label_tag(:periods), managers_periods_path
+    primary.dom_id = 'menu-id'
+    primary.dom_class = 'nav nav-tabs'
   end
 end
