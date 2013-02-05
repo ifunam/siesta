b1856c059277748601097b9e0462a21a4fad191c
Alejandro Juarez Robles <juarlex@gmail.com>
2013-02-05 09:32:30 -0600

    The name attribute is accessible for the building model

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,8 +1,9 @@
 class Building < ActiveRecord::Base
+  attr_accessible :name
   validates_presence_of :name
   validates_uniqueness_of :name
   has_many :office_cubicles
-  
+
   default_scope :order => 'name ASC'
 
 end
